9e0e66cfa82adc6d9e0b12a4a964c1776502c728
george Okello <georgeokello335@gmail.com>
2025-04-22 12:11:08 +0300

    staticfiles

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = '/login/'  # Redirect unauthorized users here
 
